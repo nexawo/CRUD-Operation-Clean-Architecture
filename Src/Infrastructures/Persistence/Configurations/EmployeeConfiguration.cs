339ea0b504da2729af75c284df575e9732271652
Nexawo <nexawo.user3@gmail.com>
2019-10-10 10:08:17 +0500

    Employee Entity Configuration Added

---
+++ Src/Infrastructures/Persistence/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nexawo.Domain.Entities.Main;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nexawo.Persistence.Configurations
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(e => e.EmployeeId)
+                .IsRequired();
+            builder.Property(e => e.FirstName)
+                .IsRequired().HasMaxLength(30);
+            builder.Property(e => e.LastName)
+                .IsRequired().HasMaxLength(30);
+            builder.Property(e => e.CNIC)
+                .IsRequired().HasMaxLength(16);
+            builder.Property(e => e.Email)
+                .IsRequired().HasMaxLength(30);
+        }
+    }
+}
---
+++ Src/Infrastructures/Persistence/Migrations/20191010050538_Configuration.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class Configuration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Employees",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CNIC",
+                table: "Employees",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CNIC",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 16);
+        }
+    }
+}
